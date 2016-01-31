16672ec7821260d59167c5571e2162dd6c9a6eaf
Sambit <codecrusher7@aol.com>
2016-01-31 23:42:16 +0100

    scratch contains test codes

---
+++ scratch/kmeans_sklearn.py
@@ -0,0 +1,143 @@
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.cluster import KMeans
+from sklearn.datasets.samples_generator import make_blobs
+
+##############################################################################
+# Generate sample data
+np.random.seed(0)
+
+batch_size = 45
+centers = [[1, 1], [-1, -1], [1, -1]]
+n_clusters = len(centers)
+X, labels_true = make_blobs(n_samples=3000, centers=centers, cluster_std=0.7)
+
+##############################################################################
+# Compute clustering with Means
+
+k_means = KMeans(init='k-means++', n_clusters=3, n_init=10)
+k_means.fit(X)
+k_means_labels = k_means.labels_
+k_means_cluster_centers = k_means.cluster_centers_
+k_means_labels_unique = np.unique(k_means_labels)
+
+##############################################################################
+# Plot result
+
+colors = ['#4EACC5', '#FF9C34', '#4E9A06']
+plt.figure()
+plt.hold(True)
+for k, col in zip(range(n_clusters), colors):
+    my_members = k_means_labels == k
+    cluster_center = k_means_cluster_centers[k]
+    plt.plot(X[my_members, 0], X[my_members, 1], 'w',
+            markerfacecolor=col, marker='.')
+    plt.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,
+            markeredgecolor='k', markersize=6)
+plt.title('KMeans')    
+plt.grid(True)
+plt.show()
+"""
+
+
+print(__doc__)
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.metrics import pairwise_distances_argmin
+from sklearn.datasets import load_sample_image
+from sklearn.utils import shuffle
+from time import time
+
+n_colors = 64
+
+# Load the Summer Palace photo
+#china = load_sample_image("china.jpg")
+
+# Convert to floats instead of the default 8 bits integer coding. Dividing by
+# 255 is important so that plt.imshow behaves works well on float data (need to
+# be in the range [0-1]
+#china = np.array(china, dtype=np.float64) / 255
+
+image = np.load('../sample-input/data.2D.npy')
+#cmap = plt.get_cmap('jet')
+
+china = np.zeros((image.shape[0], image.shape[1], 3))
+china[:,:, 0] = image[:,:]
+china[:,:, 1] = image[:,:]
+china[:,:, 2] = image[:,:]
+
+
+# Load Image and transform to a 2D numpy array.
+w, h, d = original_shape = tuple(china.shape)
+assert d == 3
+image_array = np.reshape(china, (w * h, d))
+
+
+#w = image_array.shape[0]
+#h = image_array.shape[1]
+#d = 1
+#print "w, h, d"
+#print w, h, d
+
+print("Fitting model on a small sub-sample of the data")
+t0 = time()
+image_array_sample = shuffle(image_array, random_state=0)[:1000]
+kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(image_array_sample)
+print("done in %0.3fs." % (time() - t0))
+
+# Get labels for all points
+print("Predicting color indices on the full image (k-means)")
+t0 = time()
+labels = kmeans.predict(image_array)
+print("done in %0.3fs." % (time() - t0))
+
+
+codebook_random = shuffle(image_array, random_state=0)[:n_colors + 1]
+print("Predicting color indices on the full image (random)")
+t0 = time()
+labels_random = pairwise_distances_argmin(codebook_random,
+                                          image_array,
+                                          axis=0)
+print("done in %0.3fs." % (time() - t0))
+
+
+def recreate_image(codebook, labels, w, h):
+    """Recreate the (compressed) image from the code book & labels"""
+    d = codebook.shape[1]
+    image = np.zeros((w, h, d))
+    label_idx = 0
+    for i in range(w):
+        for j in range(h):
+            image[i][j] = codebook[labels[label_idx]]
+            label_idx += 1
+    return image
+
+# Display all results, alongside original image
+plt.figure(1)
+plt.clf()
+ax = plt.axes([0, 0, 1, 1])
+plt.axis('off')
+plt.title('Original image (96,615 colors)')
+plt.imshow(china)
+
+plt.figure(2)
+plt.clf()
+ax = plt.axes([0, 0, 1, 1])
+plt.axis('off')
+plt.title('Quantized image (64 colors, K-Means)')
+#plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))
+plt.imshow(kmeans.cluster_centers_)
+
+"""
+plt.figure(3)
+plt.clf()
+ax = plt.axes([0, 0, 1, 1])
+plt.axis('off')
+plt.title('Quantized image (64 colors, Random)')
+plt.imshow(recreate_image(codebook_random, labels_random, w, h))
+"""
+
+plt.show()
